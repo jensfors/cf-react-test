c4c95787e1a86c8c5fc59e533cf1748d97f776c7
jensfors <me@jensfors.com>
2024-02-25 16:16:30 +0100

    Yayeet, just ship it

--- app/CounterExample.tsx
+++ app/CounterExample.tsx
@@ -1,17 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export const CounterExample = () => {
-  const [count, setCount] = useState(0);
-  console.log('This is count:', count);
-  return (
-    <div className="not-prose text-black flex gap-4 items-center">
-      <button
-        className="py-1 px-2 text-white bg-cyan-700 rounded"
-        onClick={() => setCount(count + 1)}
-      >
-        Increment
-      </button>
-      <p>Count: {count}</p>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	console.log('This is count:', count);
+
+	return (
+		<div>
+			<button onClick={() => setCount(count + 1)}>Increment</button>
+			<p>Count: {count}</p>
+		</div>
+	);
 };
